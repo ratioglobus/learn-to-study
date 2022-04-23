c4b9c46bbc25ed21f54338188abc2b7556099cd3
Ivan <ya@vzene.ru>
2022-04-23 17:26:02 +0300

    addstyle+js

---
+++ script.js
@@ -0,0 +1,15 @@
+alert('hello');
+
+
+
+// const encrypt  = (str) => {
+//     let result = '';
+//     let i = 0;
+  
+//     while (i < str.length) {
+//         result = `${result}${str[i + 1]}${str[i]}`;
+//         i += 2
+//     }
+//     return result.replace(/undefined/,'');
+//   };
+
