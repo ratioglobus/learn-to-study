a15a9c8e99ebf85c18ed3a3f81ffd59bf329000a
Ivan <ya@vzene.ru>
2023-01-27 23:14:35 +0300

    formProgress

--- script2.js
+++ script2.js
@@ -1,11 +1,21 @@
 function serializeForm(formNode) {
-    return new FormData(formNode)
-}
+    const { elements } = formNode
+    const data = Array.from(elements)
+      .filter((item) => !!item.name)
+      .map((element) => {
+        const { name, value } = element
   
+        return { name, value }
+      })
+  
+    console.log(data)
+}
  
+
 function handleFormSubmit(event) {
+    // Просим форму не отправлять данные самостоятельно
     event.preventDefault()
-    serializeForm(applicantForm)
+    console.log('Отправка!')
 }
   
 const applicantForm = document.getElementById('mars-once')
