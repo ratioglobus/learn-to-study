e0e47c8405e57455bbfd82c25c7bdc6eaab9ced1
Ivan <ya@vzene.ru>
2023-01-29 23:47:33 +0300

    formProgress2

--- script2.js
+++ script2.js
@@ -1,23 +1,63 @@
 function serializeForm(formNode) {
-    const { elements } = formNode
-    const data = Array.from(elements)
-      .filter((item) => !!item.name)
-      .map((element) => {
-        const { name, value } = element
-  
-        return { name, value }
-      })
-  
-    console.log(data)
+    return new FormData(formNode)
 }
- 
+
 
 function handleFormSubmit(event) {
-    // Просим форму не отправлять данные самостоятельно
     event.preventDefault()
-    console.log('Отправка!')
+    serializeForm(applicantForm)
 }
   
 const applicantForm = document.getElementById('mars-once')
 applicantForm.addEventListener('submit', handleFormSubmit)
+
+async function sendData(data) {
+    return await fetch('/api/apply/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'multipart/form-data' },
+      body: data,
+    })
+  }
+  
+  async function handleFormSubmit(event) {
+    event.preventDefault()
+    const data = serializeForm(event.target)
+  
+    toggleLoader()
+  
+    const { status, error } = await sendData(data)
+    toggleLoader()
+  
+    if (status === 200) {
+      onSuccess(event.target)
+    } else {
+      onError(error)
+    }
+  }
+  
+  
+  
+  function toggleLoader() {
+    const loader = document.getElementById('loader')
+    loader.classList.toggle('hidden')
+  }
+
+  function onSuccess(formNode) {
+    alert('Ваша заявка отправлена!')
+    formNode.classList.toggle('hidden')
+  }
+
+  function onError(error) {
+    alert(error.message)
+  }
+  
+  function checkValidity(event) {
+    const formNode = event.target.form
+    const isValid = formNode.checkValidity()
+  
+    formNode.querySelector('button').disabled = !isValid
+  }
+  
+  applicantForm.addEventListener('input', checkValidity)
+  
   
